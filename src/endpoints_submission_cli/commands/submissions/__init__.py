7b05f67e9b8ab676d8a8e890ad1ac36a0336e33c
Arav Agarwal <aravagar@umich.edu>
2026-06-12 13:05:49 -0400

    Build local creation pipeline

--- src/endpoints_submission_cli/commands/submissions/__init__.py
+++ src/endpoints_submission_cli/commands/submissions/__init__.py
@@ -6,6 +6,7 @@ import click
 
 from .add_run import submissions_add_run
 from .create import submissions_create
+from .create_local import submissions_create_local
 from .get import submissions_get
 from .list import submissions_list
 from .remove_run import submissions_remove_run
@@ -22,6 +23,7 @@ def submissions() -> None:
 
 submissions.add_command(submissions_list)
 submissions.add_command(submissions_create)
+submissions.add_command(submissions_create_local)
 submissions.add_command(submissions_get)
 submissions.add_command(submissions_update)
 submissions.add_command(submissions_withdraw)
